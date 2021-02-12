6e1047ac23b0eff062136ee3d439b6abeac01953
Duncan <50190718+skellingtonne@users.noreply.github.com>
2021-02-12 11:08:27 -0800

    Update main.c
    added description for lab document

--- Lab04/main.c
+++ Lab04/main.c
@@ -1,1 +1,21 @@
-//
+/*
+   Lab 04: Making change Functions
+   
+   Lab 4 is the creating a program that makes change using the canadian currency denominations with seperate files for functions
+    REQs
+        1) Prompt for the total purchase price
+        2) Prompt for the amount tendered
+        3) Display the total amount of change due
+        4) Compute and display the number and types of bills and coins in the change. 
+        Assume that we have the following denominations: $20, $10, $5, $2, $1, 25¢, 10¢, and 5¢.
+        5) Store all of the currency values and the denomination names in arrays.
+        6) Repeatedly use the function changeItem() in a for loop.
+        7) Consist of three files: two source files (makeChange.c, changeItem.c) and a header file
+        (changeItem.h).
+        8) Use #ifndef to prevent multiple inclusions of the header file
+        9) Remember to include your header file in your changeItem.c so that the compiler will complain (in a
+        convenient place) if you did not implement what you promised. You should always include
+    Duncan Stannard C0444224
+ 
+    Feb, 12, 2021
+*/
